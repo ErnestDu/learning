5eba12c61903b39ab55b7d7cc111a9987785c7a5
Ernest <hongqingdu@gmail.com>
2012-02-09 14:08:32 +0800

    new file:   linklist.c

---
+++ c/linklist.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+typedef int ElemType;
+
+typedef struct LinkNode {
+	ElemType data;
+	struct LinkNode *next;
+}LinkNode, *LinkList;
+
+// initialize link list
+LinkListInit()
+{
+	LinkNode *L;
+	L = (LinkNode *)malloc(sizeof(LinkNode));	// 
+	if (L == NULL)
+		printf("Memory allocation failed.\n");
+	L->next = NULL;
+}
+
+LinkList LinkListCreateH()
+{
+	LinkNode *L;
+	L = (LinkNode *)malloc(sizeof(LinkNode));
+	L->next = NULL;
+	
+	ElemType x;
+	while (scanf("%d", &x) != EOF) {
+		LinkNode *p;
+		p = (LinkNode *)malloc(sizeof(LinkNode));
+		p->data = x;
+		p->next = L->next;
+		L->next = p;
+	}
+	LinkNode *p;
+	p = L;
+	while (p->next != NULL) {
+		printf("%d ", p->data);
+		p = p->next;
+	}
+	printf("\n");
+	return L;
+}
+//void LinkListTraverse(LinkList L)
+//{
+//	LinkNode *p;
+//	p = L;
+//	while (p->next != NULL) {
+//		printf("%d ", p->data);
+//		p = p->next;
+//	}
+//	printf("\n");
+//}
+main()
+{
+	LinkList L;
+	LinkListCreateH();
+}
