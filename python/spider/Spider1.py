74d68a6ca79f87626d4778929220907fdd8312c0
Ernest <hongqingdu@gmail.com>
2012-03-12 11:09:20 +0800

    modified:   Spider1.py

--- python/spider/Spider1.py
+++ python/spider/Spider1.py
@@ -1,63 +1,46 @@
 #!/usr/bin/python
-# Filename: Spider1.py
-# Spider1.py -u url -d deep
 import os
 import sys
 import urllib
-import sqlite3
 import re
 from BeautifulSoup import BeautifulSoup
 i = 0
-while i < len(sys.argv):	## get parameters
-	if sys.argv[i] == "-u":	
+while i < len(sys.argv):
+	if sys.argv[i] == '-u':
 		url = sys.argv[i + 1]
-	elif sys.argv[i] == "-d":
+	elif sys.argv[i] == '-d':
 		deep_max = sys.argv[i + 1]
 	i += 1
+print 'url = ' + url
+print 'deep_max = ' + deep_max
 
-deep_max = int(deep_max)
 deep = 1
+deep_max = int(deep_max)
+count = 1
 def get_data(link):
 	page_data = urllib.urlopen(link).read()
 	return page_data
 
-def record_data(filename, url, page_data):
-	open(filename, 'a').write(url)
+def record_data(filename, page_data):
 	open(filename, 'a').write(page_data)
 	return 0
 
-#conn = sqlite3.connect("db")
-#c = conn.cursor()
-#c.execute('create table site (url text, data text, deep real)')
-#c.execute('insert into site values ("%s", "%s", "%d")' %(url, data, deep))
-#conn.commit()
-#c.close()
-count = 0
-soup = BeautifulSoup(get_data(url))
-filename = str(deep)
-print "deep " ,deep, "count" ,count
-record_data(filename, url, get_data(url))
-deep += 1
-count += 1
-links = soup('a', href = re.compile('http'))
-
-#for link in links:
-#	print link
-#print len(links)
-#for link in links:
-#	print link['href']
-#	record_data(str(i), get_data(link['href']))
-#	i += 1
-if deep <= deep_max:
-	count = 0
-	for link in links:
-		url = link['href']
-		print url
-		filename = str(deep)
-		print "deep " ,deep, "count" ,count
-		record_data(filename, url, get_data(url))
-		soup = BeautifulSoup(get_data(url))
-		links = links + soup('a', href = re.compile('http'))
-		count += 1
+def get_link(filename):
+	soup = BeautifulSoup(open(filename, 'r'))
 	links = soup('a', href = re.compile('http'))
+	return links['href']
+print deep, count , url
+record_data('1', get_data(url))
+deep += 1
+
+while deep < deep_max:
+	count = 1
+	soup = BeautifulSoup(open(str(deep - 1), 'r'))
+	links = soup('a', href = re.compile('http'))
+#	links = get_link(str(deep - 1))
+	for link in links:
+		print deep, count, link['href']
+		record_data(str(deep), get_data(link['href']))
+		count += 1
 	deep += 1
+
